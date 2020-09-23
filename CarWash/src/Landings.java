6b3a0f2a61388fb3168a1eb3be7d56b3095a5f1e
Leo Irakliotis <lgreco@gmail.com>
2020-09-23 11:19:49 -0500

    Event-driven example.

--- CarWash/src/Landings.java
+++ CarWash/src/Landings.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-
 /**
  * Simple demo of event-driven computing. Everything is given in a main ()
  * method for simplicity, but in a more realistic approach everything
@@ -9,10 +8,10 @@ public class Landings {
     public static void main(String[] args) {
         Random rng = new Random();
         int duration = 24*60; // 24 hours, minute by minute
-        double probablity = 0.998;
+        double probability = 0.998;
         // main loop: at every cycle we look for random events.
         for (int minute = 0; minute < duration; minute++) {
-            if ( rng.nextDouble() > probablity ) {
+            if ( rng.nextDouble() > probability ) {
                 System.out.println("There was a landing at minute: " + minute);
             }
         }
