445cf951b12ab3c85b468df8e15ef29a3d1ec24b
Leo Irakliotis <lgreco@gmail.com>
2020-09-23 11:05:58 -0500

    Simple sampe of event-driven code added (class Landings)

--- CarWash/src/Landings.java
+++ CarWash/src/Landings.java
@@ -1,24 +1,18 @@
 import java.util.Random;
 
 /**
- * Simple demo of event-driven computing
+ * Simple demo of event-driven computing. Everything is given in a main ()
+ * method for simplicity, but in a more realistic approach everything
+ * should be decomposed to dedicated methods.
  */
 public class Landings {
-
-    static Random rng = new Random();
-
-    /**
-     * Create an event at random with a prob 1%
-     * @return true if event is generated
-     */
-    static boolean landing() {
-        return rng.nextDouble() > 0.99;
-    }
-
     public static void main(String[] args) {
+        Random rng = new Random();
+        int duration = 24*60; // 24 hours, minute by minute
+        double probablity = 0.998;
         // main loop: at every cycle we look for random events.
-        for ( int minute = 0; minute < 24*60; minute++) {
-            if ( landing() ) {
+        for (int minute = 0; minute < duration; minute++) {
+            if ( rng.nextDouble() > probablity ) {
                 System.out.println("There was a landing at minute: " + minute);
             }
         }
