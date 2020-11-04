57be1e71edca16e43bc0c1190822eb05fc767115
Leo Irakliotis <lgreco@gmail.com>
2020-11-04 09:54:09 -0600

    Airplanes and hangars, oh my!

---
+++ Midterm F20/src/Practice.java
@@ -0,0 +1,9 @@
+public class Practice {
+    public static void main(String[] args) {
+
+        String s1 = "Hello";
+        String s2 = "World";
+        System.out.println((s1+s2).length());
+
+    }
+}
