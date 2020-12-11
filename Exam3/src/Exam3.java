faf0bf743a34b057aa51ad4224eff37cba3c2d58
Leo Irakliotis <lgreco@gmail.com>
2020-12-11 11:27:03 -0600

    COMP 271 final exam update 06DEC2020

---
+++ Exam3/src/Exam3.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Starter code for COMP 170 F20 003 (Leo's section), exam 3
+ */
+public class Exam3 {
+
+    public static void describeFile(Scanner s) {
+        // Work your magic here
+    } // method describeFile
+
+    public static void main(String[] args) throws IOException {
+        // The following line opens a scanner connection to a
+        // text file placed online. You need to be connected to
+        // the internet to make this work.
+        Scanner dataFrom = new Scanner(new URL("https://raw.githubusercontent.com/lgreco/DataStructures/master/Exam3/src/Exam3.txt").openStream());
+
+        // Write your code below, basically a single line call to method describeFile
+
+    }
+}
