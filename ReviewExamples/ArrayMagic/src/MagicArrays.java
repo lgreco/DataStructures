1e3e16b7f8998a67ed1e2c4c6f1232df13c8ba6b
Leo Irakliotis <lgreco@gmail.com>
2021-06-21 19:02:03 -0500

    array magic

--- ReviewExamples/ArrayMagic/src/MagicArrays.java
+++ ReviewExamples/ArrayMagic/src/MagicArrays.java
@@ -1,31 +1,37 @@
 public class MagicArrays {
 
-    /** The main field of the class **/
+    /** The main field of the class, is a string array we call "a" **/
     private String[] a = new String[0];
     /** How many elements are occupied **/
     private int size = 0;
     /** The last and currently occupied position */
     private int lastPosition = -1;
 
+    /**
+     * Method to add an element the array, increasing length (and size) by 1
+     *
+     * @param s new element added
+     */
     public void addElement(String s) {
         // Is there room in the array?
         if (size < a.length) {
             // yes there is room
-            lastPosition++;
+            lastPosition++; // update last position
             a[lastPosition] = s;
-            size++;
+            size++; // increase size
         } else {
             // nope ... expand the array, then add the element
-            String[] temporary = new String[a.length+1];
+            String[] temporary = new String[a.length+1]; // expand by 1
+            /* Copy array to temporary */
             for (int i = 0; i < a.length; i++) {
                 temporary[i] = a[i];
             }
-            a = temporary;
+            a = temporary; // put existing data to newly lengthened array
             /* Go to a's last element and populate it */
-            lastPosition = a.length-1;
-            a[lastPosition] = s;
-            size++;
-        }
+            lastPosition = a.length-1; // update last position
+            a[lastPosition] = s; // add new element at the end of array
+            size++; // increase size
+        } // method addElement
     }
 
     public void printMagicArray() {
