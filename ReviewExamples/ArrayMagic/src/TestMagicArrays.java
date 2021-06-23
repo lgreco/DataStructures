7f8c2396dac755121d335ffc1db814c5d396b3bb
Leo Irakliotis <lgreco@gmail.com>
2021-06-22 20:59:23 -0500

    Test method for magic arrays assignment

--- ReviewExamples/ArrayMagic/src/TestMagicArrays.java
+++ ReviewExamples/ArrayMagic/src/TestMagicArrays.java
@@ -1,33 +1,34 @@
+/**
+ * Testing class for the Magic Arrays assignment. The class contains just a main
+ * method that instantiates MagicArrays as object named "test". The object is
+ * populated with 16 elements. Then we conduct two tests of the contains()
+ * method. And then we conducted three tests of the removeElement() method.
+ */
 public class TestMagicArrays {
 
     public static void main(String[] args) {
+        // Instantiate test object
         MagicArrays test = new MagicArrays();
-        test.addElement("Frodo");
-        test.addElement("Sam");
-        test.addElement("Galadriel");
-        test.addElement("Gimli");
-        test.addElement("Eowen");
-        test.addElement("Faramir");
-        test.addElement("Boromir");
-        test.addElement("Pipin");
-        test.addElement("Leo");
-        test.addElement("Tom");
-        test.addElement("Gandalf");
-        test.addElement("Legolas");
-        test.addElement("Bilbo");
-        test.addElement("Denethor");
-        test.addElement("Elrond");
-        test.addElement("Saruman");
-        test.addElement("Smegol",4);
+        // Populate test object
+        test.addElement("Frodo"); test.addElement("Sam"); test.addElement("Galadriel");
+        test.addElement("Gimli"); test.addElement("Eowen"); test.addElement("Faramir");
+        test.addElement("Boromir"); test.addElement("Pipin"); test.addElement("Leo");
+        test.addElement("Tom"); test.addElement("Gandalf"); test.addElement("Legolas");
+        test.addElement("Bilbo"); test.addElement("Denethor"); test.addElement("Elrond");
+        test.addElement("Saruman"); test.addElement("Smegol",4);
+
         test.printMagicArray();
 
+        // Test the contains() method
         System.out.printf("\n\nArray contains Elrond: %b", test.contains("Elrond"));
         System.out.printf("\n\nArray contains Thomas: %b", test.contains("Thomas"));
 
+        // Test the removeElement() method
         System.out.printf("\n\nElement at position 4 removed? %b", test.removeElement(4)); // Eowen
         System.out.printf("\n\nElement Bilbo removed? %b", test.removeElement("Bilbo"));
         System.out.printf("\n\nElement Irakliotis removed? %b\n\n",test.removeElement("Irakliotis"));
 
+        // Visually verify removals
         test.printMagicArray();
     }
 }
