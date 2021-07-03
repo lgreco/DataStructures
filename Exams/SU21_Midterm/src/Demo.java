e3af54db53d2d341c286ae050585a8bc62461f49
Leo Irakliotis <lgreco@gmail.com>
2021-07-03 15:58:30 -0500

    SU21 midterm updates

---
+++ Exams/SU21_Midterm/src/Demo.java
@@ -0,0 +1,25 @@
+/**
+ * Implementation class with double-linked lists.
+ */
+public class Demo {
+    /**
+     * Main method creates an instance of the DoubleLinkedList, populates
+     * it with single-letter Strings, and then applies the display method
+     * (a midterm exam deliverable) to print the list going forward and
+     * backwards.
+     */
+    public static void main(String[] args) {
+        DoubleLinkedList demo = new DoubleLinkedList();
+        //* Being silly with for loops and char primitives
+        for (char c = 'A'; c < 'F'; c++) { // c++, get it? :-
+            // Convert char to String, make string content of new node to add to list
+            demo.add(String.valueOf(c));
+        }
+        /*
+        Call the display method. The output, when running the code, should be:
+
+        [A] [B] [C] [D] [E] [D] [C] [B] [A]
+         */
+        demo.display();
+    }
+}
