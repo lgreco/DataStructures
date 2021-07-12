7b4fcb19bca172b3fb103b51100eabfe3247b472
Leo Irakliotis <lgreco@gmail.com>
2021-07-12 16:30:25 -0500

    Simple trees

--- Trees/SimpleTrees/src/Node.java
+++ Trees/SimpleTrees/src/Node.java
@@ -1,12 +1,21 @@
+/**
+ * A very simple Node class for binary search trees.
+ */
 public class Node {
 
-    String word; // contents of a node
-    Node left; // node to the left
-    Node right; // node to the right
+    /** The node holds a string */
+    String word;
 
-    /** Basic constructor */
+    /** Pointers to left and right children */
+    Node left, right; // left and right children
+
+    
+    /**
+     * Basic constructor. Places a string in the node, and keeps both children null
+     * */
     public Node(String word) {
         this.word = word;
-    }
+        left = right = null; // Redundant but it amplifies our intention to keep these null for now.
+    } // constructor Node
 
-}
+} // class Node
