e6dfa02860920b5ddd5557ea88a6ecab4994b130
Leo Irakliotis <lgreco@gmail.com>
2021-07-22 11:32:11 -0500

    Lab for 22 JUL 21

--- ChooChoo/SLL/src/SimpleLinkedList.java
+++ ChooChoo/SLL/src/SimpleLinkedList.java
@@ -35,4 +35,43 @@ public class SimpleLinkedList {
             current.setNext(newNode); // We make the new node, the next node of what was just now the last node.
         }
     } // method addNode
-}
+
+
+    /**
+     * Stylized printing of a list, with tabulated content
+     */
+    public void displaySLL() {
+        final String NEWLINE = "\n";
+        final String TAB = "\t";
+        final String STRING_DIRECTIVE = "%s";
+        if (this.isEmpty()) {
+            System.out.printf("\n\nThe list is empty.\n\n");
+        } else {
+            System.out.printf("\n\nThe contents of the list are:");
+            String style = NEWLINE+STRING_DIRECTIVE; // Initialize the styling string for printf
+            int count = 1; // Start a count of nodes, to tabulate accordingly.
+            SimpleNode current = this.getHead(); // Get ready to traverse list beginning from its head.
+            while (current.hasNext()) {
+                System.out.printf(style, current.getContent());
+                current = current.getNext();
+                style = NEWLINE + TAB.repeat(count) + STRING_DIRECTIVE;
+                count++;
+            }
+            style += NEWLINE+NEWLINE;
+            System.out.printf(style, current.getContent());
+        }
+    } // method displaySLL
+
+
+    public static void main(String[] args) {
+        SimpleLinkedList a = new SimpleLinkedList();
+        a.addNode("Summit");
+        a.addNode("Joliet");
+        a.addNode("Dwight");
+        a.addNode("Pontiac");
+
+        SimpleLinkedList b = new SimpleLinkedList();
+        b.addNode("Chicago");
+    }
+
+} // class SimpleLinkedList
