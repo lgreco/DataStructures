283eebb1cc90f2c4e60c805c73e1105f93895f7b
Leo Irakliotis <lgreco@gmail.com>
2021-07-28 14:32:31 -0500

    Cleanup and updates prior to end of SU21

--- Hash/AlphabeticalHash/src/Implementation.java
+++ Hash/AlphabeticalHash/src/Implementation.java
@@ -1,6 +1,8 @@
 public class Implementation {
     public static void main(String[] args) {
+
         FileCabinet ourSchool = new FileCabinet(4);
+        
         ourSchool.addStudentRecord("Frodo", "Baggins", "Metallurgy");
         ourSchool.addStudentRecord("Leo", "Irakliotis", "Physics");
         ourSchool.addStudentRecord("Saruman","White", "Political Science");
@@ -8,7 +10,8 @@ public class Implementation {
         ourSchool.addStudentRecord("Tom", "Bombadil", "Cosmology");
         ourSchool.addStudentRecord("James", "Kirk", "Theater");
         ourSchool.addStudentRecord("Hans", "Solo", "Aeronautics");
-
+        ourSchool.addStudentRecord("Frazier", "Crane", "Medicine");
+        ourSchool.addStudentRecord("Paul", "Dirac", "Physics");
 
         ourSchool.printContents();
         ourSchool.showHashes();
