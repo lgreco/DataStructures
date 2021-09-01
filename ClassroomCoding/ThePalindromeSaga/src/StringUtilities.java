7463036a014b836ea83eab5e9c563ce6431b832c
Leo Irakliotis <lgreco@gmail.com>
2021-09-01 08:04:50 -0500

    Palindrome with for-loop

--- ClassroomCoding/ThePalindromeSaga/src/StringUtilities.java
+++ ClassroomCoding/ThePalindromeSaga/src/StringUtilities.java
@@ -3,9 +3,8 @@ public class StringUtilities {
     /**
      * Method that determines if a string is a palindrome.
      *
-     * The string is broken into pairs of letters symmetrically placed from its
-     * center. The pairs are compared to determine if the letters are the same.
-     * If all pairs contain the same letters, the string is a palindrome.
+     * The string is broken into pairs of letters symmetrically placed from its center. The pairs are compared to
+     * determine if the letters are the same. If all pairs contain the same letters, the string is a palindrome.
      * For example "palindrome" is broken into the following pairs:
      *   p == e ... false
      *   a == m ... false
@@ -19,7 +18,7 @@ public class StringUtilities {
      *   r == r ... true
      *   a == a ... true
      *   c == c ... true
-     * Notice that there is no pair containing the middle letter 'e'.
+     * Notice that there is no pair containing the middle letter 'e' for strings with an odd number of letters.
      *
      * @param string String to test if it is palindrome
      * @return true if palindrome; false otherwise
@@ -29,19 +28,26 @@ public class StringUtilities {
         boolean result = true;
         /* Convert to lowercase, remove everything but letters */
         string = string.toLowerCase().replaceAll("[^a-z]","");
-        /* Create a local variable with string length for readability */
+        /* Create a local variable with string length, for readability */
         int len = string.length();
-        /* Check every pair of letters, from both sides of the string */
+        /* Check every pair of letters, from both sides of the string moving towards its center */
         for (int i = 0; i < len/2; i++) {
             /* Left letter of pair */
             char leftOfCenter = string.charAt(i);
-            /* Right letter of pair */
+            /* Right letter of pair. Remember that len-1 is the last letter of a string due to 0-based counting. */
             char rightOfCenter = string.charAt(len-1-i);
-            /* Are pair letters the same? */
+            /* Are pair letters the same? True if same, false otherwise.*/
             boolean pairSame = leftOfCenter == rightOfCenter;
-            /* Update result */
+            /*
+            Update result. Remember AND operation's truth table:
+                 p      q      p && q
+                 false  false  false
+                 false  true   false
+                 true   false  false
+                 true   true   true
+             */
             result = result && pairSame;
-        }
+        } // end for
         return result;
     } // method isPalindrome
 
