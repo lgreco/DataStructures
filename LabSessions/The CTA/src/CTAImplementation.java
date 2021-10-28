beb27ee7b3adc32f51b771f00442c25f929b704e
Leo Irakliotis <lgreco@gmail.com>
2021-10-28 08:33:45 -0500

    CTA Yellow Line added

--- LabSessions/The CTA/src/CTAImplementation.java
+++ LabSessions/The CTA/src/CTAImplementation.java
@@ -6,13 +6,17 @@ public class CTAImplementation {
             "https://raw.githubusercontent.com/lgreco/DataStructures/master/data/RedLineSouthBound.txt";
     private static final String BROWN_LINE_SOUTHBOUND_TXT =
             "https://raw.githubusercontent.com/lgreco/DataStructures/master/data/BrownLineSouthBound.txt";
+    private static final String YELLOW_LINE_SOUTHBOUND_TXT =
+            "https://raw.githubusercontent.com/lgreco/DataStructures/master/data/BrownLineSouthBound.txt";
 
 
     /** Driver code */
     public static void main(String[] args) {
         CTATrainRoute redSB = new CTATrainRoute();
         CTATrainRoute brownSB = new CTATrainRoute();
+        CTATrainRoute yellowSB = new CTATrainRoute();
         redSB = redSB.buildRoute(RED_LINE_SOUTHBOUND_TXT, ALL_STATIONS_CSV);
         brownSB = brownSB.buildRoute(BROWN_LINE_SOUTHBOUND_TXT, ALL_STATIONS_CSV);
+        yellowSB = yellowSB.buildRoute(YELLOW_LINE_SOUTHBOUND_TXT, ALL_STATIONS_CSV);
     }
 }  //class CTAImplementation
