c619dc3535fa50631ccd0ddd03605fed3c30f3e7
Leo Irakliotis <lgreco@gmail.com>
2021-11-05 15:25:15 -0500

    Queues and stacks

--- LabSessions/QS/src/Stack.java
+++ LabSessions/QS/src/Stack.java
@@ -81,21 +81,4 @@ public class Stack {
         return sb.toString();
     } // method toString
 
-
-    /** Driver code */
-    public static void main(String[] args) {
-        Stack ourFirstStack = new Stack(4);
-        System.out.println(ourFirstStack);
-        ourFirstStack.push("Alex");
-        ourFirstStack.push("Paolo");
-        ourFirstStack.push("Drake");
-        ourFirstStack.push("Julia");
-        ourFirstStack.push("Juan");
-        ourFirstStack.pop();
-        ourFirstStack.pop();
-        ourFirstStack.pop();
-        System.out.println(ourFirstStack.pop());
-        System.out.println(ourFirstStack.pop());
-        System.out.println(ourFirstStack);
-    }
 }
