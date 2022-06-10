123d97e0228d8b34cc88c6b25efcd2ba999a39bd
Leo Irakliotis <lgreco@gmail.com>
2022-06-10 12:27:19 -0500

    SU 271 Dynamic Arrays

--- ClassroomCoding/Reinventing ArrayLists/src/DynamicArray.java
+++ ClassroomCoding/Reinventing ArrayLists/src/DynamicArray.java
@@ -5,6 +5,8 @@ public class DynamicArray {
 
     /** Default array size constant */
     private static final int DEFAULT_SIZE = 5;
+    /** Default resizing factor */
+    private static final int DEFAULT_RESIZE_FACTOR = 2;
 
 
     /** Underlying storage mechanism: a humble array! */
@@ -13,12 +15,30 @@ public class DynamicArray {
     private int nextAvailable;
 
 
-    /** Tell the class how to create a simple copy of itself */
-    public DynamicArray() {
-        // Initialize the array to the default size
-        aHumbleArray = new String[DEFAULT_SIZE];
-        // Pointer to next available spot in the array, to store data
+    /**
+     * Parameterized constructor. It ensures that the passed size is greater than 0.
+     * If a negative size is passed to the constructor, we use the default size
+     * for the underlying array.
+     *
+     * @param size int with desired size for the underlying array.
+     *
+     */
+    public DynamicArray(int size) {
+        // Ensure that size is > 0
+        size = (size > 0) ? size : DEFAULT_SIZE;
+        // Initialize the humble array
+        aHumbleArray = new String[size];
+        // Initialize pointer to the next available opening in the array.
         nextAvailable = 0;
+    }  // parameterized constructor
+
+
+    /**
+     * Tell the class how to create a simple copy of itself.
+     *
+     */
+    public DynamicArray() {
+        this(DEFAULT_SIZE);
     }  // default constructor
 
 
@@ -27,9 +47,11 @@ public class DynamicArray {
      *
      * Method ensures that there is always room to add the string. If the array is full,
      * the array is resized to make room.
-     * @param s
+     *
+     * @param s String to add to the dynamic array. This parameter is made final to
+     *          protect it from any accidental or intentional modification.
      */
-    public void add(String s) {
+    public void add(final String s) {
         // If we are out of room, resize first
         if (nextAvailable == aHumbleArray.length)
             resize();
@@ -42,8 +64,12 @@ public class DynamicArray {
 
     /**
      * Resizes the underlying array when we need more storage room.
+     *
+     * @param factor int multiplier for resizing the array. Must be > 1
      */
-    private void resize() {
+    private void resize(int factor) {
+        // Ensure that factor > 1
+        factor = (factor > 1) ? factor : DEFAULT_RESIZE_FACTOR;
         // Create a temporary array with more room
         String[] temporary = new String[2*aHumbleArray.length];
         // Copy the humble array contents to the temporary array
@@ -53,4 +79,12 @@ public class DynamicArray {
         aHumbleArray = temporary;
     }  // method resize
 
-}
+
+    /** Default resize method doubles the array */
+    private void resize() {
+        // Call the parameterized resize method with a factor 2
+        resize(DEFAULT_RESIZE_FACTOR);
+    }  // method resize
+
+
+}  // class DynamicArray
