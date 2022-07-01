ea9ce14176b8acef33ce659da7ea892c2d16615b
Leo Irakliotis <lgreco@gmail.com>
2022-07-01 06:25:38 -0500

    6/30 LengthLess assignment

--- Assignments/Array Stuff/src/LengthLess.java
+++ Assignments/Array Stuff/src/LengthLess.java
@@ -9,14 +9,16 @@ public class LengthLess {
         String[] test1 = {};
         String[] test2 = {"a"};
         String[] test3 = {"a", "b", "c", "d"};
+        String[] test4 = new String[5];
 
         // It's ok to use .length for testing
         boolean pass1 = (arraySize(test1) == test1.length);
         boolean pass2 = (arraySize(test2) == test2.length);
         boolean pass3 = (arraySize(test3) == test3.length);
+        boolean pass4 = (arraySize(test4) == test4.length);
 
         if (pass1 && pass2 && pass3)
-            System.out.println("\n\nYour code passes all three tests.\n");
+            System.out.println("\n\nYour code passes all four tests.\n");
         else
             System.out.println("\n\nYour code failed at least one test.\n");
     }
