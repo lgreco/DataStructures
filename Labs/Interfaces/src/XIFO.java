58fedfab624f4d2e0aa423d831c4fddcfc84a609
Leo Irakliotis <lgreco@gmail.com>
2022-07-12 15:36:23 -0500

    12JUL lab

--- Labs/Interfaces/src/XIFO.java
+++ Labs/Interfaces/src/XIFO.java
@@ -4,17 +4,47 @@ public class XIFO {
     private static final int DEFAULT_SIZE = 4;
 
     /** The underlying array of the class */
-    String[] values;
+    private String[] values;
+
+    /** How many elements are occupied? */
+    private int usage;
 
     /** Basic constructor */
     public XIFO(int size) {
         this.values = new String[size];
+        this.usage = 0;
     }
 
     /** Default constructor */
     public XIFO() {
         this(DEFAULT_SIZE);
     }
+
+    public float utilization() {
+        return ((float) this.usage)/((float) this.values.length );
+    }
+
+    /**
+     * Method to remove and return the first element of `values`.
+     */
+    public String remove() {
+        String removed = null;
+        if (this.usage > 0) {
+            // Array is not empty
+            removed = this.values[0];
+            // write a loop to move remaining elements one position to the left, and null at the end.
+        }
+        return removed;
+    }  // method remove
+
+    /** JOSE: Method to add an element at the *first available* position of `values`. */
+    public void add(String string) {
+        if (this.usage < this.values.length) {
+            // There is room for one more element
+           this.values[this.usage] = string;
+           this.usage++;
+        }
+    }  // method add
 
 
 }
