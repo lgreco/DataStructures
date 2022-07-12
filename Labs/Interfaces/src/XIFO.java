91dcc02f17514f9e3eb19648e2af04129e36a628
Leo Irakliotis <lgreco@gmail.com>
2022-07-12 15:59:21 -0500

    12JUL lab

--- Labs/Interfaces/src/XIFO.java
+++ Labs/Interfaces/src/XIFO.java
@@ -1,3 +1,7 @@
+/**
+ * A basic class for FIFO and LIFO operations
+ */
+
 public class XIFO {
 
     /** Default array size */
@@ -9,45 +13,65 @@ public class XIFO {
     /** How many elements are occupied? */
     private int usage;
 
+
     /** Basic constructor */
     public XIFO(int size) {
         this.values = new String[size];
         this.usage = 0;
     }
 
+
     /** Default constructor */
     public XIFO() {
         this(DEFAULT_SIZE);
     }
 
-    public float utilization() {
-        return ((float) this.usage)/((float) this.values.length );
-    }
 
     /**
-     * Method to remove and return the first element of `values`.
+     * Returns the fraction of used elements over total elements in the array
+     * @return a value between 0.0 and 1.0 indicating utilization of array elements.
+     */
+    public float utilization() {
+        return ((float) this.usage)/((float) this.values.length);
+    }  // method utilization
+
+
+    /**
+     * Method to remove and return the first element of array values.
      */
     public String remove() {
         String removed = null;
+        // Operate on non-empty lists.
         if (this.usage > 0) {
             // Array is not empty
             removed = this.values[0];
-            // DUE THU 7/14: write a loop to move remaining elements one position to the left, and null at the end.
+            // DUE THU 7/14:
+            // write a loop to move remaining elements one position to the left,
+            // and null at the end.
         }
         return removed;
     }  // method remove
 
-    /** JOSE: Method to add an element at the *first available* position of `values`. */
+
+    /**
+     * Adds an element at the *first available* position of array values.
+     * Method checks to ensure that there is room for the new element.
+     *
+     * @param string to add to the array
+     */
     public void add(String string) {
-        if (this.usage < this.values.length) {
-            // There is room for one more element
-           this.values[this.usage] = string;
-           this.usage++;
-        }
+        if (this.usage < this.values.length)
+            // There is room: add the element and increment usage
+           this.values[this.usage++] = string;
     }  // method add
 
 
-    /** FATEHA: Method to add an element at the first position (`[0]`) of `values`. */
+    /**
+     * Adds an element at the first position (`[0]`) of array values.
+     * Method checks to ensure that there is room for the new element.
+     *
+     * @param String to add to the array.
+     */
     public void firstElement(String string) {
         if (this.usage < this.values.length) {
             // There is room to add the new string
