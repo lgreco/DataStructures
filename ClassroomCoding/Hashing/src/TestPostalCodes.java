9f697b2e5e13e4cef7c3086172d511ee87fa90e0
Leo Irakliotis <lgreco@gmail.com>
2022-07-21 19:59:32 -0500

    Last homework

--- ClassroomCoding/Hashing/src/TestPostalCodes.java
+++ ClassroomCoding/Hashing/src/TestPostalCodes.java
@@ -1,17 +1,8 @@
-public class TestPostalCodes {
+public class TestPostalCodes  {
 
-    public static void main(String[] args) {
-        PostalCodes demo = new PostalCodes(5);
-        System.out.println(demo);
-        demo.put(60601, "Chicago");
-        demo.put(60605, "Chicago");
-        demo.put(60602, "Oak Park");
-        demo.put(60602, "Oak Park");
-        demo.put(62704, "Springfield");
-        demo.put(60156, "Lake in the Hills");
-        demo.put(54648, "Washington");
-        demo.put(60601, "New York");
-        System.out.println(demo);
-    }
+    /*
+    test code to validate your work will be available 12 noon on
+    Friday 7/22/22.
+     */
 
 }
