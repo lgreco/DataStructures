ac187175ba42e61f9ce1dadd07f65d69b43ad8d4
Leo Irakliotis <lgreco@gmail.com>
2022-07-22 11:27:58 -0500

    Test code for PostalCodes

--- ClassroomCoding/Hashing/src/TestPostalCodes.java
+++ ClassroomCoding/Hashing/src/TestPostalCodes.java
@@ -1,8 +1,31 @@
 public class TestPostalCodes  {
 
-    /*
-    test code to validate your work will be available 12 noon on
-    Friday 7/22/22.
-     */
+    private static int count(int zipCode, PostalCodes postalCodes) {
+        int counter = 0;
+        for (Zip zip:
+                postalCodes.getCodes()) {
+            if (zip != null) {
+                Zip cursor = zip;
+                while (cursor != null) {
+                    if (cursor.getZipCode() == zipCode)
+                        counter++;
+                    cursor = cursor.getNext();
+                }
+            }
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        PostalCodes demo = new PostalCodes(4);
+        demo.put(60601, "Chicago");
+        demo.put(60601, "Chicago");
+        if (count(60601,demo) > 1)
+            System.out.println("\nTest failed: your structure allows duplicate zip codes.");
+        else if (count(60601, demo) == 1)
+            System.out.println("\nTest passes: your structure seems to be working as specified");
+        else
+            System.out.println("\nTest failed: your structure doesn't accept unique keys.");
+    }
 
 }
