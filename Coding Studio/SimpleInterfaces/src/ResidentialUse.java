549cc572a1536de08a76677fd2dabcd3593d9189
Leo Irakliotis <lgreco@gmail.com>
2022-10-21 14:08:32 -0500

    Coding Studio 10/21/22

--- Coding Studio/SimpleInterfaces/src/ResidentialUse.java
+++ Coding Studio/SimpleInterfaces/src/ResidentialUse.java
@@ -1,3 +1,44 @@
 public interface ResidentialUse{
 
+    /**
+     * @return boolean, true if place has a basement
+     */
+    public boolean hasBasement();
+
+
+    /**
+     * @return boolean, true if place has a garage
+     */
+    public boolean hasGarage();
+
+
+    /**
+     * @return int, count of bedrooms in the place
+     */
+    public int numberOfBedrooms();
+
+
+    /**
+     * @return double, number of bathrooms
+     */
+    public double numberOfBathrooms();
+
+
+    /**
+     * return int, square footage of the place
+     */
+    public int squareFootage();
+
+
+    /**
+     * Causes the specified room to be redecorated
+     */
+    public void redecorate(String room);
+
+
+    /**
+     * @return boolean, place has a/c
+     */
+    public boolean hasAirConditioning();
+
 }
