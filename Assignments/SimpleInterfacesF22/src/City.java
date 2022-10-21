6b39695111bcc519bd18fa97df4566922e0850a1
Leo Irakliotis <lgreco@gmail.com>
2022-10-21 18:30:18 -0500

    Midterm assignment solutions

--- Assignments/SimpleInterfacesF22/src/City.java
+++ Assignments/SimpleInterfacesF22/src/City.java
@@ -1,6 +1,6 @@
 public class City extends Place implements Local, Statistical, Transportation {
 
-    // Populate this clas with the fields and the methods necessary to
+    // Populate this class with the fields and the methods necessary to
     // fulfill the implemented interfaces.
 
 }  // class City
