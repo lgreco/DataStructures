87207d1d2dfbcfc887c1b3568d1c080835296afa
Leo Irakliotis <lgreco@gmail.com>
2022-10-21 12:43:19 -0500

    Coding Studio 10/21/22

--- Coding Studio/SimpleInterfaces/src/ResidentialUse.java
+++ Coding Studio/SimpleInterfaces/src/ResidentialUse.java
@@ -1,37 +1,3 @@
 public interface ResidentialUse{
 
-    /**
-     * @return int with number of bedrooms in the object
-     */
-    public int numberOfBedrooms();
-
-
-    /**
-     * @return int with count of full bathrooms in the object
-     */
-    public int numberOfFullBathrooms();
-
-
-    /**
-     * @return int with count of half bathrooms in the object
-     */
-     public int numberOfHalfBathrooms();
-
-
-    /**
-     * @return int with number of outlets in the object
-     */
-    public int numberOfOutlets();
-
-
-    /**
-     * @return boolean true if fancy appliances
-     */
-    public boolean areAppliancesStainlessSteel();
-
-
-    /**
-     * @return int with number of cars fitting in garage
-     */
-    public int garageSpaces();
 }
--- Coding Studio/SimpleInterfaces/src/SingleFamilyHome.java
+++ Coding Studio/SimpleInterfaces/src/SingleFamilyHome.java
@@ -1,2 +1,3 @@
-public class SingleFamilyHome extends Building implements ResidentialUse{
+public class SingleFamilyHome extends Building implements ResidentialUse {
+
 }
