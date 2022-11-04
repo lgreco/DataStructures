30b4f274c46f5ae2821b759bb75688f23f945397
Leo Irakliotis <lgreco@gmail.com>
2022-11-04 18:03:08 -0500

    Testing for the enhanced Hammock hotel

--- Coding Studio/Hammocks/src/TestHammockHotel.java
+++ Coding Studio/Hammocks/src/TestHammockHotel.java
@@ -5,4 +5,11 @@ public class TestHammockHotel {
     TESTING WILL BE AVAILABLE AFTER 11 AM SATURDAY 11/5/22
 
      */
+
+    public static void main(String[] args) {
+
+        // Create the test object
+        NFS_HammockHotel test = new NFS_HammockHotel(2, 2.0, 2);
+    }
+
 }
